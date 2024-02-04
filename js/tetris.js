e2f7d875835b2355d6036607a40a7c782865de14
twingay96 <flavonoid37@gmail.com>
2024-02-04 21:44:32 +0900

    키 입력에따라 블럭이동 구현 (~30:51)

--- js/tetris.js
+++ js/tetris.js
@@ -13,7 +13,7 @@ let tempMovingItem; // 무빙을 실제로실행하기 전에 잠깐 담아두�
 const BLOCKS= {
     tree: [
         // 4개의 배열은 특정 블럭을 회전시켰을때 나올 수 있는 4개의 모양 ( direction )
-        [[0,0],[0,1],[1,0],[1,1]],
+        [[2,1],[0,1],[1,0],[1,1]],
         [],
         [],
         [],
@@ -25,7 +25,8 @@ const movingItem = {
     type: "tree",
     direction: 0, // 화살표 방향 위를 눌렀을때 회전시키는용도
     top: 0,
-    left: 0,
+    left: 3,
+    // top와 left를 통해서 블럭의 위치를 조정, 방향키를 입력시 변화하게끔
 };
 
 init()
@@ -35,7 +36,6 @@ function init(){
     // 처음 script가 호출이 될때 실행되는 함수
     tempMovingItem = {...movingItem};
     // 스프레드 오퍼레이터를 통해서 변수를 담으면 movingItem내부의 값만 tempMovingItem에 저장됨
-
     for(let i = 0; i < GAME_ROWS; i++) {
         prependNewLine()
     }
@@ -57,16 +57,35 @@ function renderBlocks(){
     // BLOCKS의 모양대로 렌더링해주는 역할
     //  tempMovingItem 내부의 속성들을 바로 바로 사용할 수 있는 변수로 선언
     const { type, direction, top, left } = tempMovingItem;
-    console.log(BLOCKS[type][direction])
-    BLOCKS[type][direction].forEach(block => {
-        const x = block[0];
-        const y = block[1];
-        console.log({playground})
-        const target = playground.childNodes[y].childNodes[0].childNodes[x];
-        console.log(target)
-        target.classList.add(type)
-    
+    const movingBlocks = document.querySelectorAll(".moving");
+    movingBlocks.forEach(moving=>{
+        moving.classList.remove(type, "moving");
     })
 
+    BLOCKS[type][direction].forEach(block => {
+        const x = block[0] + left;
+        const y = block[1] + top;
+        const target = playground.childNodes[y].childNodes[0].childNodes[x];
+        target.classList.add(type, "moving")
     
+    })
+}
+
+document.addEventListener("keydown", e=> { 
+    switch(e.keyCode){
+        case 39:
+            moveBlock("left", 1);
+            break;
+        case 37:
+            moveBlock("left", -1);
+            break;
+        default:
+            break;
+    }
+})
+
+function moveBlock(moveType, amount ){
+    // tempMovingItem 내부의 top, left 등을 변경시켜줌
+    tempMovingItem[moveType] += amount;
+    renderBlocks()
 }
